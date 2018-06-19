1839acdae966bc9bf2c5f88b0eb444230fc47d92
Allan Peres <allan.peres@db1.com.br>
2018-06-19 14:58:23 -0300

    #Compilação do projeto pelo docker e utilização do h2

--- cars/src/main/java/com/allanperes/mentoria/cars/service/CarService.java
+++ cars/src/main/java/com/allanperes/mentoria/cars/service/CarService.java
@@ -1,19 +1,44 @@
 package com.allanperes.mentoria.cars.service;
 
 import com.allanperes.mentoria.cars.model.Car;
+import com.allanperes.mentoria.cars.model.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+
+import static java.lang.String.format;
 
 @Service
 public class CarService {
 
+    @Autowired
+    private CarRepository carRepository;
+
     public List<Car> findAll() {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Buggatti", 2017L, "Black"));
-        cars.add(new Car("Ferrari", 1991L, "Red"));
-        return cars;
+        return carRepository.findAll();
     }
 
+    public Car findById(Long id) {
+        return carRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(format("Não encontrado carro com o id %s", id)));
+    }
+
+    public List<Car> findCarByBrand(String brand) {
+        return carRepository.findByBrand(brand);
+    }
+
+    public Car save(Car car) {
+        return carRepository.save(car);
+    }
+
+    public String delete(Long id) {
+        try {
+            carRepository.delete(findById(id));
+            return "Carro deletado com sucesso";
+        } catch (Exception e) {
+            return "Não foi possível deletar o carro";
+        }
+    }
 }
---
+++ cars/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into car
+values (1, 'Buggatti', 'Black', 2017);
+
+insert into car
+values (2, 'Ferrari', 'Red', 1995);
+
+insert into parts
+values (1, 'motor', 3000, 1, 'V8');
+
+insert into parts
+values (2, 'lanterna', 250, 13, 'xenom 2000');
+
+insert into parts
+values (3, 'volante', 450, 12, 'normal');
